eda6f4d551a50b75ccefbd529d40b1ab2f9efffc
apros7 <lucas.vilsen@gmail.com>
2023-02-19 14:47:17 +0100

    mask model now works

--- Use&FineTuneModels/maskModel.py
+++ Use&FineTuneModels/maskModel.py
@@ -1,2 +1,18 @@
 # https://huggingface.co/tasks/fill-mask
 # Could potentially train own model or use pretrained danish model for mask
+
+from transformers import pipeline
+
+# Load the pre-trained Danish language model
+model_name = "Maltehb/danish-bert-botxo"
+fill_mask = pipeline("fill-mask", model=model_name)
+
+# Input sentence with a mask token
+input_sentence = "Hej mit [MASK] er Lucas"
+
+# Use the model to predict the missing word
+predictions = fill_mask(input_sentence)
+
+# Print the top 5 predictions with their scores
+for pred in predictions[:5]:
+    print(pred["token_str"], pred["score"])
