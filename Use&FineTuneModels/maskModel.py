38653c14aefda645d5ad1263a06acc96e1835b32
apros7 <lucas.vilsen@gmail.com>
2023-02-20 06:57:36 +0100

    code added for adding mask model to main.py - more testing needed

--- Use&FineTuneModels/maskModel.py
+++ Use&FineTuneModels/maskModel.py
@@ -16,3 +16,25 @@ predictions = fill_mask(input_sentence)
 # Print the top 5 predictions with their scores
 for pred in predictions[:5]:
     print(pred["token_str"], pred["score"])
+    # pred["token_str"] is string
+
+# add this to bottom of find_suggestions:
+"""
+    start = time.time()
+    word2 = mask_model_word(word1 + " [MASK] " + word3)
+    mask_model_time.append(time.time() - start)
+
+    #print("NGRAM: got this word: ", word)
+    #print("MASK:  got this word: ", word2)
+    return word
+
+model_name = "Maltehb/danish-bert-botxo"
+fill_mask = pipeline("fill-mask", model=model_name)
+
+def mask_model_word(sentence):
+    predictions = fill_mask(sentence)
+    lst = []
+    for pred in predictions[:5]:
+        lst.append(pred["token_str"])
+    return lst
+"""
