b01512a7ef9f196191a48e11c3c5d4db583af46b
apros7 <lucas.vilsen@gmail.com>
2023-03-05 21:46:08 +0100

    cleaned main script

--- GrammatiktakBackend/main.py
+++ GrammatiktakBackend/main.py
@@ -1,16 +1,19 @@
+# allennlp 
+
 # initailize the tracker
 from Timetracking.timetracker import TimeTracker
 time_tracker = TimeTracker()
-# time_tracker.inactive = True
+#time_tracker.inactive = True
 
 # import internal modules
 from Punctuation.correct_punctuation import PunctuationCorrector
 from Spellchecking.tagger import Tagger
 from Spellchecking.capitalized import CapitalizationCorrector
-from Utilities.utils import prepare_sentence, concat_errors, move_index_based_on_br
+from Spellchecking.misspelled_words import MisspelledWordsCorrector
+from Spellchecking.wrong_tense import TenseCorrector
+from Utilities.utils import prepare_sentence, concat_errors
 
 # importing external modules
-# from polyleven import levenshtein # add "polyleven" to requirements.txt if needed
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -20,36 +23,43 @@ time_tracker.track("import modules")
 punctuation_corrector = PunctuationCorrector()
 capitalize_corrector = CapitalizationCorrector()
 tagger = Tagger()
+misspellings_corrector = MisspelledWordsCorrector()
+tense_corrector = TenseCorrector()
 
 time_tracker.track("initialize correctors")
+
+# corrector function
 
 def correct_input(input):
     punctuation_errors = punctuation_corrector.correct_punctuation(input)
     time_tracker.track("correct punctuation")
+
     pos_tags, ner_tags = tagger.get_tags(input)
     time_tracker.track("get tags")
+
     capitalization_errors = capitalize_corrector.correct_capitalization(input, pos_tags, ner_tags)
     time_tracker.track("correct capitalization")
+
     return concat_errors(punctuation_errors + capitalization_errors)
 
+# flask app:
+
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://grammatiktak.com/"])
 @app.route("/", methods=["POST"])
 
 def index():
     data = request.get_json()
     input = data["sentence"]
     output = correct_input(input)
-    #print(*output, sep="\n")
     return jsonify(output)
 
 time_tracker.complete_reset()
 
 message = "hey Christian<br>tak for Det. Jeg er desværre I skole til, kl 18, så det har Lucas ikke mulighed for.<br>Jeg håber, at i får en dejlig aften :smile:."
 #message = "En anden form for bias er confirmation bias, hvor man som forsker vægte undersøgelser som understøtte ens hypotse end undersøgelser som vil modsige ens hypotese. Det omfatter også, at hvis man har en vis forventning af et bestemt præparat virkning, at man i så fald også vil fortolke ens data på en måde som understøtter ens forventning. Confirmation bias kan også påvirke ens testpersoner, hvis man ikke er opmærksom på dette. Fx hvis man giver en testperson et præparat som testpersonen forventer har en effekt, vil dette kunne påvirke testpersonens opfattelse af stoffets virkning, på en måde som igen understøtter ens forventning. I det sidstnævnte eksempel er det placeboeffekten som vil kunne give patienten en fornemmelse af at præparatet virker selvom det ikke nødvendigvis er tilfældet. For at modvirker confirmation bias kan man foretage sig af blinding i tre forskellige grader. Ved almindelig blinding ved selve deltagerne i studiet ikke om de modtager den aktuelle behandling eller om de ikke gør, fx ved at give en kalkpille eller lign. Dette er med til at modvirke patientens egne forventninger til behandlingen. Dertil er der også dobbeltblinding hvor hverken patienten eller personalet ved om den behandling de får/giver er den faktiske behandling eller blot placebo. Dette er med til at modvirke at personalets forventning til behandlingen videregives ubevidst under kommunikation. Til sidst kan man også tripelblinde, der lægges til de to tidligere med at dem som behandlinger og analyser data fra studiet ikke ved hvilken gruppe som har modtaget den faktiske behandling. Disse tre former for blinding bidrager til at mindske mængden af confirmation bias mest muligt."
-prepared = " ".join(prepare_sentence(message))
-errors1 = correct_input(message)
-print(*errors1, sep="\n")
+#errors1 = misspellings_corrector.test_spellchecker()
+#print(*errors1, sep="\n")
 
 time_tracker.track2("end")
 time_tracker(.5)
