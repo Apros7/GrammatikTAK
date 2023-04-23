f6182591ac6125a5593c2a838b869af5c87068ee
apros7 <lucas.vilsen@gmail.com>
2023-04-23 11:15:27 +0200

    made is_healthy

--- GrammatiktakBackend/Utilities/error_handling.py
+++ GrammatiktakBackend/Utilities/error_handling.py
@@ -1,7 +1,7 @@
 ## Script for working with errors
 
 class Error():
-    def __init__(self, wrong_word: str, right_word: str, indexes: list, description: str) -> None:
+    def __init__(self, wrong_word: str = None, right_word: str = None, indexes: list = None, description: str = None) -> None:
         self.wrong_word = wrong_word
         self.right_word = right_word
         self.indexes = indexes
@@ -10,6 +10,14 @@ class Error():
     def get_description(self):
         return self.description.replace("wrong", self.wrong_word).replace("right", self.right_word)
 
+    def is_healthy(self):
+        if len([var_name for var_name, var_value in self.__dict__.items() if var_value is None]) == 0:
+            return True
+        return False
+
     def to_list(self):
-        return [self.wrong_word, self.right_word, self.indexes, self.get_description()]
+        if self.is_healthy():
+            return [self.wrong_word, self.right_word, self.indexes, self.get_description()]
+        missing_instance_variables = [var_name for var_name, var_value in self.__dict__.items() if var_value is None]
+        raise NotImplementedError(f"Error is not healthy. Please fill these variables: {[missing_instance_variables]}")
 
