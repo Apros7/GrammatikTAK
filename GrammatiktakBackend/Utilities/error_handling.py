c134315b2ec681634e9044409be20e44ecca6735
apros7 <lucas.vilsen@gmail.com>
2023-05-05 06:59:58 +0200

    more error handling methods

--- GrammatiktakBackend/Utilities/error_handling.py
+++ GrammatiktakBackend/Utilities/error_handling.py
@@ -1,3 +1,13 @@
+
+class ErrorList():
+    def __init__(self, lst) -> None:
+        self.errors = lst
+
+    def __add__(self, other):
+        if not isinstance(other, ErrorList):
+            return NotImplemented
+
+
 class Error():
     def __init__(self, wrong_word: str = None, right_word: str = None, indexes: list = None, description: str = None) -> None:
         self.wrong_word = wrong_word
@@ -25,8 +35,4 @@ class Error():
             return [self.wrong_word, self.right_word, self.indexes, self.get_description()]
         missing_instance_variables = [var_name for var_name, var_value in self.__dict__.items() if var_value is None]
         raise NotImplementedError(f"Error is not healthy. Please fill these variables: {[missing_instance_variables]}")
-
-    def __add__(self, other):
-        if not isinstance(other, Error):
-            return NotImplemented
         
