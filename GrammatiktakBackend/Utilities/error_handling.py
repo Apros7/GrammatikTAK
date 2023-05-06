3d4775faaa5e5ee26b777173138e33b45d7d0560
apros7 <lucas.vilsen@gmail.com>
2023-05-06 06:53:59 +0200

    finished error handling tools

--- GrammatiktakBackend/Utilities/error_handling.py
+++ GrammatiktakBackend/Utilities/error_handling.py
@@ -1,26 +1,144 @@
+from collections import defaultdict
+
+approved_types = ["add_punc", "del_punc", "det", "add_cap", "del_cap", "nutids-r"]
+error_types_to_concat = ["add_punc", "del_punc", "add_cap", "del_cap"]
+
+def init_dict(self):
+    def def_value():
+        return []
+    return defaultdict(def_value)
+
+def errors_to_index_dict(errors):
+    dict = init_dict()
+    for error in errors:
+        dict[error[2]] += error
+    return dict
+
+def add_punc(error, error2):
+    wrong_word = error.wrong_word
+    right_word = error.right_word + error2.right_word[-1]
+    indexes = error.indexes
+    description = error.description + " " + error2.description
+    return Error(wrong_word, right_word, indexes, description, error.get_type())
+
+def del_punc(error, error2):
+    wrong_word = error.wrong_word
+    right_word = error.right_word[:-1]
+    indexes = error.indexes
+    description = error.description + " " + error2.description
+    return Error(wrong_word, right_word, indexes, description, error.get_type())
+
+def add_cap(error, error2):
+    wrong_word = error.wrong_word
+    right_word = error.right_word.capitalize()
+    indexes = error.indexes
+    description = error.description + " " + error2.description
+    return Error(wrong_word, right_word, indexes, description, error.get_type())
+
+def del_cap(error, error2):
+    wrong_word = error.wrong_word
+    right_word = error.right_word.lower()
+    indexes = error.indexes
+    description = error.description + " " + error2.description
+    return Error(wrong_word, right_word, indexes, description, error.get_type())
+
+def project_error(errors, error_to_project):
+    project_type = error_to_project.get_type()
+    if project_type not in error_types_to_concat:
+        raise NotImplementedError("Trying to project an error of type {}, which is not allowed.".format(project_type))
+
+    error_projectors = {
+        "add_punc": add_punc,
+        "del_punc": del_punc,
+        "add_cap": add_cap,
+        "del_cap": del_cap,
+    }
+
+    projected_errors = []
+
+    for error in errors:
+        projected_errors.append(error_projectors[project_type](error, error_to_project))
+    
+    return projected_errors
+
+
+def error_concatenator(errors, errors_to_project_onto_others):
+
+    """
+    Return sorted, listed and concatenated ErrorList
+    """
+
+    if not isinstance(errors, ErrorList) or not isinstance(errors_to_project_onto_others, ErrorList):
+        return NotImplementedError("Can only work with ErrorLists")
+
+    errors_dict = errors_to_index_dict(errors)
+    errors_to_project_dict = errors_to_index_dict(errors_to_project_onto_others)
+
+    for key in errors_to_project_dict.keys():
+        if key not in errors_dict.keys():
+            continue
+        for error_to_project in errors_to_project_dict[key]:
+            projected_errors = project_error(errors_dict[key], error_to_project)
+            errors_dict[key] = projected_errors
+
+    return ErrorList(errors_dict).to_list()
 
 class ErrorList():
     def __init__(self, lst) -> None:
         self.errors = lst
+        if not self.is_healthy():
+            raise ValueError(f"ErrorList is not healthy. Some errors have missing values.")
+
+    def is_healthy(self):
+        return any([error.is_healthy() for error in self.errors])
+
+    def sort(errors):
+        return sorted(errors, key=lambda x: x[2][1])
+
+    def init_dict(self):
+        def def_value():
+            return []
+        return defaultdict(def_value)
+    
+    def concat_errors(self, errors):
+        concated_errors = []
+        dict = self.init_dict()
+        for error in errors:
+            dict[error[2]] += error
+        for lst_of_errors in dict.values():
+            if len(lst_of_errors) < 2:
+                continue
+            for error in lst_of_errors:
+
+                concated_errors.append(error)
 
     def __add__(self, other):
         if not isinstance(other, ErrorList):
-            return NotImplemented
-
+            return NotImplementedError("Can only add ErrorList + ErrorList")
+        all_errors = self.errors + other.errors
+        concated_errors = self.concat_errors(all_errors)
+        return ErrorList(concated_errors)
+    
+    def to_list(self):
+        return list(self.sort([error.to_list() for error in self.errors]))
 
 class Error():
-    def __init__(self, wrong_word: str = None, right_word: str = None, indexes: list = None, description: str = None) -> None:
+    def __init__(self, wrong_word: str = None, right_word: str = None, indexes: list = None, description: str = None, type: str = None) -> None:
         self.wrong_word = wrong_word
         self.right_word = right_word
         self.indexes = indexes
         self.description = description
-        self.type = None
+        self.set_type(type)
 
     def set_type(self, type):
-        approved_types = ["punc", "determinant", "capitalization", "nutids-r"]
-        if type not in approved_types:
+        if type is None:
+            pass
+        elif type not in approved_types:
             raise ValueError(f"Type must be in {approved_types}")
-        self.type = type
+        self.__type = type
+
+    def get_type(self):
+        return self.__type
 
     def get_description(self):
         return self.description.replace("wrong", self.wrong_word).replace("right", self.right_word)
