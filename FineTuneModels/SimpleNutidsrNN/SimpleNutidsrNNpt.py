0dcacd77df58fa13f63d1dd22bfb167a05f3df2c
apros7 <lucas.vilsen@gmail.com>
2023-06-03 21:52:37 +0200

    added pt module

--- FineTuneModels/SimpleNutidsrNN/SimpleNutidsrNNpt.py
+++ FineTuneModels/SimpleNutidsrNN/SimpleNutidsrNNpt.py
@@ -1,5 +1,4 @@
-from tinygrad.tensor import Tensor
-from tinygrad.nn import optim
+import torch.nn as nn
 import torch
 import os
 import pandas as pd
@@ -36,21 +35,27 @@ class NutidsrTokenizer():
     def _one_hot_encode(self, number):
         return [1 if i == number else 0 for i in range(18)]
 
-class NutidsrModel():
-
+class NutidsrModel(nn.Module):
     def __init__(self) -> None:
+        super(NutidsrModel, self).__init__()
         number_of_unique_pos = 18
         number_of_pos_including_padding = 21
-        self.l1 = Tensor.scaled_uniform(number_of_pos_including_padding*number_of_unique_pos, 256)
-        self.l2 = Tensor.scaled_uniform(256, 256)
-        self.l3 = Tensor.scaled_uniform(256, 64)
-        self.l4 = Tensor.scaled_uniform(64, 1)
+        self.l1 = nn.Linear(number_of_pos_including_padding*number_of_unique_pos, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, 64)
+        self.l4 = nn.Linear(64, 1)
+        self.activation = nn.LeakyReLU(0.2)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = x.dot(self.l1).leakyrelu(0.2)
-        x = x.dot(self.l2).leakyrelu(0.2)
-        x = x.dot(self.l3).leakyrelu(0.2)
-        x = x.dot(self.l4).sigmoid()
+        x = self.l1(x)
+        x = self.activation(x)
+        x = self.l2(x)
+        x = self.activation(x)
+        x = self.l3(x)
+        x = self.activation(x)
+        x = self.l4(x)
+        x = self.sigmoid(x)
         return x
 
 tokenizer = NutidsrTokenizer()
@@ -60,10 +65,15 @@ print("Tokenizing test...")
 x_test_tokenized = list(tokenizer(X_test))
 print("Done Tokenizing.")
 
-EPOCHS, BATCH_SIZE = 10, 32
+EPOCHS, BATCH_SIZE = 10, 128
 
 model = NutidsrModel()
-optimizer = optim.Adam(optim.get_parameters(model),lr=0.0002, b1=0.5)
+optimizer = torch.optim.Adam(model.parameters(),lr=0.0002, betas=(0.5, 0.999))
+device = "cpu"
+loss_fn = nn.MSELoss()
+torch.device(device)
+model.to(device)
+
 
 n_steps = len(x_train_tokenized) // BATCH_SIZE
 eval_steps = len(x_test_tokenized) // BATCH_SIZE
@@ -78,19 +88,19 @@ def get_batch(x, y):
     return x_batch, y_batch
 
 def train_model(xb, yb):
-    yb = Tensor([yb])
+    yb = torch.tensor(yb, dtype=torch.float32)
     optimizer.zero_grad()
     output = model.forward(xb)
-    loss = (output * yb).mean()
+    loss = loss_fn(output, yb)
     loss.backward()
     optimizer.step()
-    return loss.cpu().numpy()
+    return loss
 
 def test_model():
     accuracies = []
     for i in tqdm(range(eval_steps)):
         xb, yb = get_batch(x_test_tokenized, y_test)
-        xb = Tensor(xb, requires_grad=False)
+        xb = torch.tensor(xb)
         output = list(model.forward(xb))
         accuracy = sum([1 if to_binary(o.numpy()) == y else 0 for (o,y) in zip(output, yb)])/len(output)
         accuracies.append(accuracy)
@@ -101,29 +111,29 @@ def to_binary(o):
     return 1 if o > 0.5 else 0
 
 def save_model(epoch):
-    os.makedirs("simpleNNmodels", exist_ok=True)
-    params = optim.get_parameters(model)
-    with open("simpleNNmodels/nn1_epoch:"+str(epoch)+".pkl", "wb") as f:
-        pickle.dump(params, f)
+    os.makedirs("simpleNNmodelsPT", exist_ok=True)
+    torch.save(model.state_dict(), f"simpleNNmodelsPT/model_{epoch}.pt")
 
 print("Accuracy before training: ")
 test_model()
-#save_model(-1)
+save_model(-1)
 
 
 for epoch in range(EPOCHS):
+    model.train()
     for i in tqdm(range(n_steps)):
         xb, yb = get_batch(x_train_tokenized, y_train)
-        xb = Tensor(xb, requires_grad=True)
+        xb = torch.tensor(xb)
         loss = train_model(xb, yb)
         if i % 3000 == 0:
             print(f"Loss at step {i}: {-loss}")
     
     print(f"Done with Epoch {epoch}")
     print("Evaluating...")
+    model.eval()
 
     test_model()
-    #save_model(epoch)
+    save_model(epoch)
 
         
 
