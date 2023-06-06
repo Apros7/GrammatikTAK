671a17bfd83e7e7764caf040d53d6f435fc66a67
apros7 <lucas.vilsen@gmail.com>
2023-06-06 12:06:49 +0200

    added composite words

--- GrammatiktakBackend/Spellchecking/spelling_errors.py
+++ GrammatiktakBackend/Spellchecking/spelling_errors.py
@@ -4,22 +4,12 @@ from Utilities.error_handling import Error, ErrorList
 import pickle
 import pandas as pd
 
-"""
-To do:
-- Composite words
-- More than one spelling mistake? Currently only corrects one spelling mistake
-- Correct words but in a place where it does not make sense (jeg heder lucas)
-"""
-
-hard_coded_frequent_spelling_errors = {
-    "idag": ["i dag"],
-    "imorgen": ["i morgen"],
-    "igår": ["i går"],
-    "igang": ["i gang"],
-}
 
 NO_CORRECTION_IF_IN_WORD = "-_"
 PARTLY_CLEANING = ",.:;?!()[]{}'\""
+
+def load_spelling_errors():
+    return {k: v for k,v in zip(pickle.load(open("Datasets/spelling_errors_keys.pickle", "rb")), pickle.load(open("Datasets/spelling_errors_values.pickle", "rb")))}
 
 class SpellChecker():
 
@@ -29,13 +19,23 @@ class SpellChecker():
 
     def __init__(self) -> None:
         print("Loading spellchecking dictionaries...")
+        self.composite_words = pickle.load(open("Datasets/composite_words.pickle", "rb"))
         self.dictionary = pickle.load(open("Datasets/dictionary.pickle", "rb"))
-        self.spelling_errors = {**hard_coded_frequent_spelling_errors, **pickle.load(open("Datasets/misspellings_dict.pickle", "rb"))}
+        self.spelling_errors = load_spelling_errors()
+        print(len(self.spelling_errors))
 
-    def is_word_in_dictionary(self, word): return not word in self.dictionary
+    def is_word_in_dictionary(self, word): return word in self.dictionary
     def punctuation_in_word(self, word): return any([x in word for x in NO_CORRECTION_IF_IN_WORD])
     def partly_clean_sentence(self, sent): return ''.join(char for char in sent if char not in PARTLY_CLEANING)
-    def has_possible_correct(self, word): return word in self.spelling_errors
+    def has_possible_misspelling_correction(self, word): return word in self.spelling_errors
+
+    def get_composite_combinations(self, word): 
+        return [word[:i] + word[i+1:] for i in range(len(word)) if word[i] == " "] + [word[:i] + " " + word[i:] for i in range(len(word))]
+
+    def composite_words_error(self, word): 
+        for composite_word in self.get_composite_combinations(word): 
+            if composite_word in self.composite_words: return (True, composite_word)
+        return (False, None)
 
     def create_error_message(self, wrong_word, correct_word, all_words_from_sentence, index_of_word_in_all_words) -> list:
         error_type = "spellcheck"
@@ -46,8 +46,10 @@ class SpellChecker():
     
     def correct_word(self, word, index, words):
         if self.punctuation_in_word(word): return None
-        if not self.is_word_in_dictionary(word): return None
-        if not self.has_possible_correct(word): return None
+        if self.is_word_in_dictionary(word): return None
+        is_composite_error, composite_word = self.composite_words_error(word)
+        if is_composite_error: return self.create_error_message(word, composite_word, words, index)
+        if not self.has_possible_misspelling_correction(word): return None
         return self.create_error_message(word, self.spelling_errors[word], words, index)
 
     def correct(self, input):
@@ -55,4 +57,3 @@ class SpellChecker():
         cleaned_words = prepare_sentence(self.partly_clean_sentence(input), lowercase=True)
         errors = ErrorList([self.correct_word(word, index, words) for index, word in enumerate(cleaned_words)])
         return errors
-
