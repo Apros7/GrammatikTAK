0d18817cfd47460ad8a605f4b25deaedbbd569b9
apros7 <lucas.vilsen@gmail.com>
2023-06-15 15:47:14 +0200

    updated docker ignore and spellings_errors to get rid of composite errors

--- GrammatiktakBackend/Spellchecking/spelling_errors.py
+++ GrammatiktakBackend/Spellchecking/spelling_errors.py
@@ -31,14 +31,6 @@ class SpellChecker():
     def has_possible_misspelling_correction(self, word): return word in self.spelling_errors
     def word_in_ner_tags(self, word_index, ner_tags): return any([word_index == ner_index for ner_index in ner_tags]) # Should probably be changed
 
-    def get_composite_combinations(self, word): 
-        return [word[:i] + word[i+1:] for i in range(len(word)) if word[i] == " "] + [word[:i] + " " + word[i:] for i in range(len(word))]
-
-    def composite_words_error(self, word): 
-        for composite_word in self.get_composite_combinations(word): 
-            if composite_word in self.composite_words: return (True, composite_word)
-        return (False, None)
-
     def create_spellchecking_error_message(self, wrong_word, correct_word, all_words_from_sentence, index_of_word_in_all_words) -> list:
         error_type = "spellcheck"
         ## When frontend can take lists change this:
