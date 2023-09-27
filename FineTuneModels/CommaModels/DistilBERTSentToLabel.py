c5f9c08f3191150fc610c36d4c51b2f4d2a2a16a
apros7 <lucas.vilsen@gmail.com>
2023-09-27 11:18:06 +0200

    faster encoding

--- FineTuneModels/CommaModels/DistilBERTSentToLabel.py
+++ FineTuneModels/CommaModels/DistilBERTSentToLabel.py
@@ -19,17 +19,18 @@ tokenizer = AutoTokenizer.from_pretrained("Geotrend/distilbert-base-da-cased")
 ## CHANGE THIS ##
 # os.chdir("/Users/lucasvilsen/Desktop/GrammatikTAK/Datasets")
 # df = pd.read_csv("SentToLabel_15-5_Revisited_small.csv", sep=";")
+
 df = pd.read_csv("SentToLabel_15-5_Revisited.csv", sep=";")
 print(len(df))
 
-# df = df[:2500]
 # df = df[:28000000] # should not be active
+# df = df[:400000]
 print(len(df))
 
 data = df["data"].to_list()
 labels = df["label"].to_list()
 
-del df
+df = 0
 
 class CustomDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels=None, batch_size=16):
@@ -50,17 +51,51 @@ print("Splitting data")
 ## CHANGE THIS ##
 test_size = 0.05
 X_train, X_val, y_train, y_val = train_test_split(data, labels, test_size=test_size, random_state=1212)
+print("Len x train: ", len(X_train))
 
 data = 0
 labels = 0
 
 start_time = time.time()
 print("Tokenizing val:")
-X_val_tokenized = tokenizer(X_val, padding=True, truncation=True)
+
+# X_val_tokenized2 = {"attention_mask": [], "input_ids": []}
+# batch_size = 64
+
+# for i in tqdm(range(0, len(X_val), batch_size), desc="Tokenizing"):
+#     tokenized_text = tokenizer(X_val[i:i+batch_size], padding=True, truncation=True)
+#     for k in tokenized_text:
+#         X_val_tokenized2[k].extend(tokenized_text[k])
+
+X_val_tokenized = tokenizer.batch_encode_plus(X_val, padding=True, truncation=True)
+
+# for k, v in X_val_tokenized2.items():
+#     print(v == X_val_tokenized[k])
+#     # print(v)
+#     # print(X_val_tokenized[k])
+
+# print(X_val_tokenized.keys())
+
 X_val = 0
 print("Time taken for validation: ", time.time() - start_time)
 print("Expected total time ", ((time.time() - start_time) * 1/test_size) // 60, "min", ((time.time() - start_time) * 1/test_size) % 60, "sek")
-X_train_tokenized = tokenizer(X_train, padding=True, truncation=True)
+
+start_time = time.time()
+X_train_tokenized = tokenizer.batch_encode_plus(X_train, padding=True, truncation=True)
+print("Len x train tokenized: ", len(X_train_tokenized))
+print(time.time() - start_time)
+
+# X_train_tokenized2 = {"attention_mask": [], "input_ids": []}
+# batch_size = 64
+
+# for i in tqdm(range(0, len(X_train), batch_size), desc="Tokenizing"):
+#     tokenized_text = tokenizer(X_train[i:i+batch_size], padding=True, truncation=True)
+#     for k in tokenized_text:
+#         X_train_tokenized2[k].extend(tokenized_text[k])
+
+# for k, v in X_train_tokenized2.items():
+#     print(v == X_train_tokenized[k])
+
 print("Done with tokenization")
 X_train = 0
 
@@ -70,6 +105,9 @@ batch_size = 32
 epochs = 2
 
 train_dataset = CustomDataset(X_train_tokenized, y_train, batch_size=batch_size)
+print("Len x train dataset: ", len(train_dataset))
+# print("Supposed train steps: ", len(train_dataset) * epochs // batch_size)
+# print("Supposed train steps: ", (factor * len(train_dataset) * epochs // batch_size) / 7)
 val_dataset = CustomDataset(X_val_tokenized, y_val, batch_size=batch_size)
 ## CHANGE THESE ##
 # os.chdir("/Users/lucasvilsen/Desktop/GrammatikTAK/FineTuneModels/CommaModels")
